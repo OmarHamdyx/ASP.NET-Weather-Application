2bcd18ed12386d362cce2df719f2dfb932cd8b3d
OmarHamdyx <zoltarscorpio@gmail.com>
2024-03-26 02:09:23 +0200

    Update Code.

---
+++ WeatherApplication/Controllers/CityWeatherDetailsController.cs
@@ -0,0 +1,24 @@
+﻿using Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Services;
+
+namespace WeatherApplication.Controllers
+{
+    [Controller]
+    public class CityWeatherDetailsController : Controller
+    {
+        private readonly ISetViewBag _setViewBag;
+
+		public CityWeatherDetailsController(ISetViewBag setViewBag) 
+        {
+			_setViewBag = setViewBag;
+		}
+        [Route("/weather/{cityCode}")]
+        public IActionResult ReturnCityWeatherDetails(string? cityCode)
+        {
+            ViewBag.Title = _setViewBag;
+			return View(null,cityCode);
+        }
+
+    }
+}
---
+++ WeatherApplication/Program.cs
@@ -0,0 +1,13 @@
+using Interfaces;
+using Microsoft.Extensions.Options;
+using Services;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllersWithViews();
+builder.Services.AddTransient<ISetViewBag, SetViewBag>();
+builder.Services.AddTransient<ICitiesProvider,CitiesProvider>();
+var app = builder.Build();
+app.MapControllers();   
+app.UseStaticFiles();
+app.UseRouting();   
+app.Run();
